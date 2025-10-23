39784e06fde4a426988355be4665e042edecfb0c
Ross Golder <ross@golder.org>
2025-10-23 22:40:30 +0700

    Run make fmt to format code

--- routers/api/v1/repo/actions_run.go
+++ routers/api/v1/repo/actions_run.go
@@ -495,8 +495,6 @@ func getRunJobsAndCurrent(ctx *context.APIContext, runID, jobIndex int64) (*acti
 	return jobs[0], jobs, nil
 }
 
-
-
 // LogCursor represents a log cursor position
 type LogCursor struct {
 	Step     int   `json:"step"`
